e122ef9ab6fc0baadd95f13e9be34342b961525c
SoldovskijBB <soldovskij@hotmail.com>
2017-02-26 10:36:22 +0300

    Make HMR only on debug mode

--- webpack.config.js
+++ webpack.config.js
@@ -4,10 +4,6 @@ const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const DEBUG             = process.env.NODE_ENV !== 'production';
 
 let webpackConfig = {
-	entry: [
-		'webpack-hot-middleware/client',
-		path.join(__dirname, 'frontend', 'index')
-	],
 	output: {
 		path: path.join(__dirname, 'public'),
 		filename: 'main.js',
@@ -46,11 +42,18 @@ let webpackConfig = {
 };
 
 if (DEBUG) {
+	webpackConfig.entry = [
+		'webpack-hot-middleware/client',
+		path.join(__dirname, 'frontend', 'index')
+	];
 	webpackConfig.devtool = 'source-map';
 	webpackConfig.plugins.push(
 		new webpack.HotModuleReplacementPlugin()
 	);
 } else {
+	webpackConfig.entry = [
+		path.join(__dirname, 'frontend', 'index')
+	];
 	webpackConfig.plugins.push(
 		new webpack.optimize.UglifyJsPlugin(),
 		new webpack.DefinePlugin({
